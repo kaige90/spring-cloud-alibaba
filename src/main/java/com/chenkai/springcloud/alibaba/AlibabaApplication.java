540b9c82be66ffc036537380c7c1ee42f6809214
chenkai <kaige90@163.com>
2019-01-20 18:50:00 +0800

    Add springCloudAlibaba示例项目初始化

---
+++ src/main/java/com/chenkai/springcloud/alibaba/AlibabaApplication.java
@@ -0,0 +1,14 @@
+package com.chenkai.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaApplication.class, args);
+    }
+
+}
+
